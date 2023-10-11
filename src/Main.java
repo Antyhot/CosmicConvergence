1f35bc4507240f121d0f7103f8fff7bb3c6aa4df
Амир Нурмухамбетов <thereug6@gmail.com>
2023-10-11 15:28:50 +0200

    Basic game loop implemented;

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import javax.swing.JFrame;
+
+/**
+ * Main class for the game.
+ */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setTitle("Cosmic Convergence");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
     }
 }
